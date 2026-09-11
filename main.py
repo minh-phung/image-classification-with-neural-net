6435112740bc921c1cbc49a6d7aa398ab94d699d
minh phung <minh_phung@outlook.com>
2026-09-11 17:30:42 -0400

    edit

--- main.py
+++ main.py
@@ -391,11 +391,11 @@ for each_num_conv_lay in [num_conv_lay[0]]:
                 Path(dir_out).mkdir(parents = True, exist_ok = True)
                 
                 
-                learn_rate_val = 5e-3
+                learn_rate_val = 10e-4
                 print("learn rate", learn_rate_val)
                 
-                '''
-                for each_seed in range(0, 3):
+                
+                for each_seed in range(3, 7):
                     
                     print("seed", each_seed)
                     
@@ -422,7 +422,7 @@ for each_num_conv_lay in [num_conv_lay[0]]:
                     )
                     
                     time.sleep(10)
-                '''
+                
                 
                 plot.variation(
                     dir = dir_out,
